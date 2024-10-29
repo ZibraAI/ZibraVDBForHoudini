6437938660e36beef203cddac69487f3f094006f
maletsden <maletsden@gmail.com>
2024-10-29 18:14:03 +0200

    Fixed string dealocation

--- src/ROP/ROP_ZibraVDBCompressor.cpp
+++ src/ROP/ROP_ZibraVDBCompressor.cpp
@@ -366,7 +366,7 @@ namespace Zibra::ZibraVDBCompressor
                     continue;
                 }
 
-                providedChannelNames.push_back(channelNameStr);
+                providedChannelNames.emplace_back(UT_String::ALWAYS_DEEP, channelNameStr);
 
                 CompressionEngine::ZCE_CompressionSettingsPerChannel perChannelSettings{};
                 perChannelSettings.quality = static_cast<float>(evalFloat(qualityParamNameStr.c_str(), 0, tStart));
