dc4f655130e9d8b18640f67b4f89a747f29b50a9
Dmytro Bulatov <dmytro.bulatov@boolka.dev>
2024-11-15 17:28:58 +0900

    Updated nodes versions. Updated HDA name.

--- src/openvdb/OpenVDBEncoder.h
+++ src/openvdb/OpenVDBEncoder.h
@@ -9,6 +9,7 @@ namespace Zibra::OpenVDBSupport
     public:
         static openvdb::GridPtrVec EncodeFrame(const CompressionEngine::ZCE_FrameInfo& frameInfo,
                                                const CompressionEngine::ZCE_DecompressedFrameData& frameData) noexcept;
+
     private:
         static bool IsTransformEmpty(const CompressionEngine::ZCE_Transform& gridTransform);
         static openvdb::math::Transform::Ptr OpenVDBTransformFromMatrix(const CompressionEngine::ZCE_Transform& gridTransform);
