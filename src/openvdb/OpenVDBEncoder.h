4a89be1cdfcc0f7c18e6beb5d038269f3df544fb
Danil Andrieiev <danssg08@gmail.com>
2025-01-24 15:36:53 +0200

    [SKD] Updates

--- src/openvdb/OpenVDBEncoder.h
+++ src/openvdb/OpenVDBEncoder.h
@@ -9,16 +9,21 @@ namespace Zibra::OpenVDBSupport
         int32_t offsetZ = 0;
     };
 
+    struct DecompressedFrameData
+    {
+        CE::Decompression::SpatialBlock* spatialBlocks;
+        CE::Decompression::ChannelBlock* channelBlocks;
+    };
+
     class OpenVDBEncoder
     {
     public:
-        static openvdb::GridPtrVec EncodeFrame(const CompressionEngine::ZCE_FrameInfo& frameInfo,
-                                               const CompressionEngine::ZCE_DecompressedFrameData& frameData,
+        static openvdb::GridPtrVec EncodeFrame(const CE::Decompression::FrameInfo& frameInfo,
+                                               const DecompressedFrameData& frameData,
                                                const EncodeMetadata& encodeMetadata) noexcept;
-
     private:
-        static bool IsTransformEmpty(const CompressionEngine::ZCE_Transform& gridTransform);
-        static openvdb::math::Transform::Ptr OpenVDBTransformFromMatrix(const CompressionEngine::ZCE_Transform& gridTransform);
+        static bool IsTransformEmpty(const Math3D::Transform& gridTransform);
+        static openvdb::math::Transform::Ptr OpenVDBTransformFromMatrix(const Math3D::Transform& gridTransform);
         static void OffsetTransform(openvdb::math::Transform::Ptr gridTransform, const EncodeMetadata& encodeMetadata);
     };
 } // namespace Zibra::OpenVDBSupport
