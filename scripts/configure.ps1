879b7d3230e2ef020d6c9c4306605755eb8cf9ad
Dmytro Bulatov <dmytro.bulatov@boolka.dev>
2025-02-08 23:30:30 +0900

    Removed explicit compiler specification on Linux

--- scripts/configure.ps1
+++ scripts/configure.ps1
@@ -13,11 +13,6 @@ if ($IsWindows)
    $AdditionalArgs += " -T v143,host=x64,version=14.35"
 }
 
-if ($IsLinux)
-{
-   $AdditionalArgs += " -DCMAKE_C_COMPILER=gcc-11 -DCMAKE_CXX_COMPILER=g++-11"
-}
-
 Invoke-Expression "cmake -S . -B $TargetFolder $AdditionalArgs"
 
 Pop-Location
