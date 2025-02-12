21e5526725a10dc51aff82c79a309fca9fd83f6b
maletsden <maletsden@gmail.com>
2025-02-12 16:02:34 +0200

    Decompression in chunks implementation

--- src/bridge/RHIWrapper/RHIWrapper.h
+++ src/bridge/RHIWrapper/RHIWrapper.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Zibra/RHI.h>
+
 #include "openvdb/OpenVDBEncoder.h"
 
 namespace Zibra
@@ -20,18 +21,20 @@ namespace Zibra
         void StopRecording() const noexcept;
         void GarbageCollect() noexcept;
 
-        RHI::RHIRuntime* const GetRHIRuntime() const noexcept;
+        [[nodiscard]] RHI::RHIRuntime* const GetRHIRuntime() const noexcept;
 
-        const CE::Decompression::DecompressorResources& GetDecompressorResources() const noexcept;
+        [[nodiscard]] const CE::Decompression::DecompressorResources& GetDecompressorResources() const noexcept;
 
-        OpenVDBSupport::DecompressedFrameData GetDecompressedFrameData(const CE::Decompression::FrameInfo& frameInfo) const noexcept;
+        [[nodiscard]] OpenVDBSupport::DecompressedFrameData GetDecompressedFrameData(
+            const CE::Decompression::DecompressFrameDesc& desc,
+            const CE::Decompression::DecompressedFrameFeedback& feedback) const noexcept;
+        static void ReleaseDecompressedFrameData(OpenVDBSupport::DecompressedFrameData& decompressedFrameData) noexcept;
 
     private:
         RHI::RHIFactory* m_RHIFactory = nullptr;
         RHI::RHIRuntime* m_RHIRuntime = nullptr;
 
-        CE::Decompression::DecompressorResources m_decompressorResources;
+        CE::Decompression::DecompressorResources m_decompressorResources{};
     };
-
 
 } // namespace Zibra
--- src/openvdb/OpenVDBEncoder.h
+++ src/openvdb/OpenVDBEncoder.h
@@ -18,12 +18,15 @@ namespace Zibra::OpenVDBSupport
     class OpenVDBEncoder
     {
     public:
-        static openvdb::GridPtrVec EncodeFrame(const CE::Decompression::FrameInfo& frameInfo,
-                                               const DecompressedFrameData& frameData,
+        static openvdb::GridPtrVec CreateGrids(const CE::Decompression::FrameInfo& frameInfo,
                                                const EncodeMetadata& encodeMetadata) noexcept;
+        static void EncodeFrame(openvdb::GridPtrVec& grids, const CE::Decompression::DecompressFrameDesc& desc,
+                                const CE::Decompression::DecompressedFrameFeedback& feedback, const DecompressedFrameData& frameData,
+                                const EncodeMetadata& encodeMetadata) noexcept;
+
     private:
         static bool IsTransformEmpty(const Math3D::Transform& gridTransform);
         static openvdb::math::Transform::Ptr OpenVDBTransformFromMatrix(const Math3D::Transform& gridTransform);
-        static void OffsetTransform(openvdb::math::Transform::Ptr gridTransform, const EncodeMetadata& encodeMetadata);
+        static void OffsetTransform(openvdb::math::Transform::Ptr& gridTransform, const EncodeMetadata& encodeMetadata);
     };
 } // namespace Zibra::OpenVDBSupport
