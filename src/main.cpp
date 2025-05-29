0ea28500b070409ff4f1c9f2a3ce6fcdde6be878
skaplunzibra <s.kaplun@zibra.ai>
2025-05-29 11:04:22 +0300

    Base LOP implementation

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,10 @@
 // This header must be included exactly once in the plugin!
 #include <UT/UT_DSOVersion.h>
 
+#include "LOP/LOP_ZibraVDBDecompressor.h"
 #include "ROP/ROP_ZibraVDBCompressor.h"
 #include "SOP/SOP_ZibraVDBDecompressor.h"
+#include "LOP/ZibraVDBPluginRegistry.h"
 
 extern "C" SYS_VISIBILITY_EXPORT void newSopOperator(OP_OperatorTable* table)
 {
@@ -14,9 +16,23 @@ extern "C" SYS_VISIBILITY_EXPORT void newSopOperator(OP_OperatorTable* table)
     table->addOperator(new ZibraVDBDecompressor::SOP_ZibraVDBDecompressor_Operator());
 }
 
+extern "C" SYS_VISIBILITY_EXPORT void newLopOperator(OP_OperatorTable* table)
+{
+    using namespace Zibra;
+    
+    table->addOperator(new ZibraVDBDecompressor::LOP_ZibraVDBDecompressor_Operator());
+}
+
 extern "C" SYS_VISIBILITY_EXPORT void newDriverOperator(OP_OperatorTable* table)
 {
     using namespace Zibra;
 
     table->addOperator(new ZibraVDBCompressor::ROP_ZibraVDBCompressor_Operator(ContextType::OUT));
+}
+
+SYS_VISIBILITY_EXPORT void newGeometryPrim(OP_OperatorTable* table)
+{
+    using namespace Zibra;
+
+    ZibraVDBPluginRegistry::GetInstance().Initialize();
 }
