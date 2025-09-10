4fd88cc949d4a487065ecd006d9cefa680799331
skaplunzibra <s.kaplun@zibra.ai>
2025-09-10 12:43:55 +0300

    Fixed the win build

--- common/PrecompiledHeader.h
+++ common/PrecompiledHeader.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// Standard library includes
+// Standard library
 #include <cassert>
 #include <chrono>
 #include <climits>
@@ -10,7 +10,6 @@
 #include <cstdlib>
 #include <execution>
 #include <filesystem>
-#include <fstream>
 #include <iostream>
 #include <map>
 #include <mutex>
@@ -20,7 +19,6 @@
 #include <sstream>
 #include <string>
 #include <thread>
-#include <unordered_map>
 #include <unordered_set>
 #include <utility>
 #include <vector>
@@ -29,10 +27,53 @@
 #include <bit>
 #endif
 
-// Platform specific includes
 #if ZIB_TARGET_OS_WIN
 #include <codecvt>
 #include <locale>
+#endif
+
+// Houdini includes
+#include <CH/CH_LocalVariable.h>
+#include <CMD/CMD_Manager.h>
+#include <GA/GA_Attribute.h>
+#include <GA/GA_AttributeType.h>
+#include <GA/GA_GBMacros.h>
+#include <GEO/GEO_PrimPoly.h>
+#include <GU/GU_Detail.h>
+#include <GU/GU_PrimVDB.h>
+#include <OP/OP_AutoLockInputs.h>
+#include <OP/OP_Director.h>
+#include <OP/OP_Operator.h>
+#include <OP/OP_OperatorTable.h>
+#include <PRM/PRM_Conditional.h>
+#include <PRM/PRM_Include.h>
+#include <PRM/PRM_Parm.h>
+#include <PRM/PRM_ParmList.h>
+#include <PRM/PRM_SpareData.h>
+#include <PRM/PRM_TemplateBuilder.h>
+#include <ROP/ROP_Error.h>
+#include <ROP/ROP_Node.h>
+#include <ROP/ROP_Templates.h>
+#include <SOP/SOP_Node.h>
+#include <SYS/SYS_Math.h>
+#include <UT/UT_Assert.h>
+#include <UT/UT_Exit.h>
+#include <UT/UT_IOTable.h>
+#include <UT/UT_Interrupt.h>
+#include <UT/UT_OFStream.h>
+#include <UT/UT_StringHolder.h>
+
+// OpenVDB includes
+#include <openvdb/io/Stream.h>
+#include <openvdb/openvdb.h>
+#include <openvdb/tools/GridTransformer.h>
+#include <openvdb/tools/Interpolation.h>
+
+// 3rd party includes
+#include <json.hpp>
+
+// Platform specific includes
+#if ZIB_TARGET_OS_WIN
 #define WIN32_LEAN_AND_MEAN
 #define NOATOM
 #define NOGDI
@@ -66,50 +107,6 @@
 #error Unexpected OS
 #endif
 
-// Houdini includes (common to all components)
-#include <UT/UT_String.h>
-#include <UT/UT_Assert.h>
-#include <UT/UT_Exit.h>
-#include <UT/UT_IOTable.h>
-#include <UT/UT_Interrupt.h>
-#include <UT/UT_OFStream.h>
-#include <UT/UT_StringHolder.h>
-
-// Additional Houdini includes for main project
-#include <CH/CH_LocalVariable.h>
-#include <CMD/CMD_Manager.h>
-#include <GA/GA_Attribute.h>
-#include <GA/GA_AttributeType.h>
-#include <GA/GA_GBMacros.h>
-#include <GEO/GEO_PrimPoly.h>
-#include <GU/GU_Detail.h>
-#include <GU/GU_PrimVDB.h>
-#include <OP/OP_AutoLockInputs.h>
-#include <OP/OP_Director.h>
-#include <OP/OP_Operator.h>
-#include <OP/OP_OperatorTable.h>
-#include <PRM/PRM_Conditional.h>
-#include <PRM/PRM_Include.h>
-#include <PRM/PRM_Parm.h>
-#include <PRM/PRM_ParmList.h>
-#include <PRM/PRM_SpareData.h>
-#include <PRM/PRM_TemplateBuilder.h>
-#include <ROP/ROP_Error.h>
-#include <ROP/ROP_Node.h>
-#include <ROP/ROP_Templates.h>
-#include <SOP/SOP_Node.h>
-#include <SYS/SYS_Math.h>
-
-// OpenVDB includes
-#include <openvdb/io/Stream.h>
-#include <openvdb/openvdb.h>
-#include <openvdb/tools/GridTransformer.h>
-#include <openvdb/tools/Interpolation.h>
-
-// 3rd party includes
-#include <json.hpp>
-
-// Zibra SDK includes
 #define ZRHI_NO_STATIC_API_DECL
 #include <Zibra/RHI.h>
 
